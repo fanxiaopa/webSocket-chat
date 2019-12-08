0075f3ab7906d09ea918c2f044349abdfec8ce14
fanxiaopa <1437606887@qq.com>
2019-12-08 16:54:40 +0800

    点对点聊天/2019/12/8

---
+++ server/serverChat.js
@@ -0,0 +1,43 @@
+var ws = require("nodejs-websocket");
+console.log("开始建立连接...")
+
+var chat1 = null,chat2 = null , chat1Ready = false , chat2Ready = false;
+var server = ws.createServer(function(conn){
+    conn.on("text", function (str) {
+         
+         console.log("收到的信息为:"+str)
+        if(str==="A online1"){
+            chat1 = conn;
+            chat1Ready = true;
+           // conn.sendText("success");
+        }
+        if(str==="B online2"){
+            chat2 = conn;
+            chat2Ready = true;
+        }
+
+        if(chat1Ready&&chat2Ready){
+            var code=str.substr(-1,1)
+            str=str.slice(0,-1)
+            console.log(code)
+            
+            if(code==='1'){
+                chat1.sendText("--->A说 :"+str);
+                chat2.sendText("--->A说 :"+str);
+            }else{
+                chat1.sendText("--->B说 :"+str);
+                chat2.sendText("--->B说 :"+str);
+            }
+           
+        }
+
+       // conn.sendText(str)
+    })
+    conn.on("close", function (code, reason) {
+        console.log("关闭连接")
+    });
+    conn.on("error", function (code, reason) {
+        console.log("异常关闭")
+    });
+}).listen(8001)
+console.log("WebSocket建立完毕")
